81932f13136b59cd72063a4e48480c2af88812b2
petinari <petinari@outlook.com>
2022-12-04 21:50:14 -0400

    Add project files.

---
+++ MVC_2022_Macoratti/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_2022_Macoratti.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
